2078f66092940fb06fbb997d3128920abc8362ac
Jonathan Robichaud <jonathanrobichaud93@gmail.com>
2015-02-04 16:06:04 -0400

    Create bench.sh

---
+++ bench.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+./benchmark.out 160 > results/result160.txt
+./benchmark.out 320 > results/result320.txt
+./benchmark.out 480 > results/result480.txt
+./benchmark.out 640 > results/result640.txt
+./benchmark.out 800 > results/result800.txt
+./benchmark.out 960 > results/result960.txt
+./benchmark.out 1120 > results/result1120.txt
+./benchmark.out 1280 > results/result1280.txt
+./benchmark.out 1440 > results/result1440.txt
+./benchmark.out 1600 > results/result1600.txt
+./benchmark.out 1760 > results/result1760.txt
+./benchmark.out 1920 > results/result1920.txt
+./benchmark.out 2080 > results/result2080.txt
+./benchmark.out 2240 > results/result2240.txt
+./benchmark.out 2400 > results/result2400.txt
+./benchmark.out 2560 > results/result2560.txt
